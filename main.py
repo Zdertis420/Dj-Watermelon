cb3aafbec9a3d9013f3890954282f7170923a367
MAKILLL <92119511+MAKILLL@users.noreply.github.com>
2023-04-23 20:52:51 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 import logging
+
+from aiogram.dispatcher.filters import Text
+
 import validator
 from time import sleep
 from aiogram import Bot, Dispatcher, executor, types
-from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup
+from aiogram.types import ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardButton, InlineKeyboardMarkup, CallbackQuery
 
 with open('token') as token:
     TOKEN = token.read()
@@ -18,6 +21,13 @@ dispatcer = Dispatcher(dj)
 keyboard = ReplyKeyboardMarkup(resize_keyboard=True)
 keyboard.add(KeyboardButton('start'))
 
+genres = InlineKeyboardMarkup(row_width=2)
+n = 0
+for btn in ["Джаз", "Классическая Музыка", "Поп-музыка", "Рок/металл", "Хип-хоп", "Шансон"]:
+    n += 1
+    button = InlineKeyboardButton(text=btn,
+                                  callback_data=f"genre_{n}")
+    genres.add(button)
 
 
 
@@ -32,6 +42,32 @@ async def start(mess: types.Message):
     await dj.send_message(text='1. Как настроение? Весело, грустно, или может ты словил дзен?\n'
                                '2. Чо делаешь?\n',
                           chat_id=mess.from_user.id)
+
+
+@dispatcer.message_handler()
+async def askGenre(mess: types.Message):
+    await dj.send_message(text='И последний, какой жанр предпочетаешь?',
+                          chat_id=mess.from_user.id,
+                          reply_markup=genres)
+
+
+@dispatcer.callback_query_handler(Text(startswith="genre_"))
+async def callbackGenre(call: CallbackQuery):
+    await call.answer('Хорошо')
+    action = call.data.split("_")[1]
+    if action == "1":
+        print("Джаз")
+    if action == "2":
+        print("Классическая Музыка")
+    if action == "3":
+        print("Поп-музыка")
+    if action == "4":
+        print("Рок/металл")
+    if action == "5":
+        print("Хип-хоп")
+    if action == "6":
+        print("Шансон")
+
 
 
 @dispatcer.message_handler()
