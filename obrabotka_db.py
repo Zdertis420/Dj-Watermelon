a4f1ee33e0a9eca0347ad95457c4ea157186208a
MAKILLL <92119511+MAKILLL@users.noreply.github.com>
2023-04-26 22:23:27 +0300

    Update obrabotka_db.py

--- obrabotka_db.py
+++ obrabotka_db.py
@@ -3,15 +3,11 @@ import random
 
 
 def recomend(li, Uid):
-    M = ["Весёлое", "Спокойное", "Грустное"]
-    O = ["Тренируюсь", "Отдыхаю", "В дороге", "Работаю-Учусь"]
-    G = ["Джаз", "Классическая Музыка", "Поп-музыка", "Рок-металл", "Хип-хоп", "Шансон"]
-
     m = li[Uid][0]
     o = li[Uid][1]
     g = li[Uid][2]
 
-    DB = "music/DJ-Арбуз музыка.db"
+    DB = "watermelow.db"
     connection = sqlite3.connect(DB)
     cur = connection.cursor()
 
@@ -22,33 +18,73 @@ def recomend(li, Uid):
         table = "t" + str(table[0][0])
     result = cur.execute(f"""SELECT song FROM {table}""").fetchall()
     random.shuffle(result)
+
     n = 3
     songs = result[:n]
     for i in range(len(songs)):
         songs[i] = songs[i][0]
 
-    answer = "Тогда тебе стоит послушать это:\n\n"
-    n = 0
+    answer = ""
+    k = 0
     for i in songs:
-        n += 1
+        k += 1
         if i != songs[-1]:
-            answer += str(n) + ". " + i + "\n"
+            answer += str(k) + ". " + i + "\n"
         else:
-            answer += str(n) + ". " + i
+            answer += str(k) + ". " + i
 
     return answer
 
-    # n += 3
-    # if n == 21:
-    #     print("Нет")
-    # else:
-    #     songs = result[n-3:n]
-    #     for i in range(len(songs)):
-    #         songs[i] = songs[i][0]
-    #
-    #     answer = ""
-    #     for i in songs:
-    #         if i != songs[-1]:
-    #             answer += str(n) + ". " + i + "\n"
-    #         else:
-    #             answer += str(n) + ". " + i
+
+def recomendRM():
+    M = ["Весёлое", "Спокойное", "Грустное"]
+    O = ["Тренируюсь", "Отдыхаю", "В дороге", "Работаю-Учусь"]
+    G = ["Джаз", "Классическая Музыка", "Поп-музыка", "Рок-металл", "Хип-хоп", "Шансон"]
+
+    m = random.choice(M)
+    o = random.choice(O)
+    g = random.choice(G)
+
+    DB = "watermelow.db"
+    connection = sqlite3.connect(DB)
+    cur = connection.cursor()
+
+    table = cur.execute(f"""SELECT id FROM main WHERE mood='{m}' AND occupation='{o}' AND genre='{g}'""").fetchall()
+    if table[0][0] < 10:
+        table = "t" + "0" + str(table[0][0])
+    else:
+        table = "t" + str(table[0][0])
+    result = cur.execute(f"""SELECT song FROM {table}""").fetchall()
+
+    songs = result[:5]
+    answer = "Ну вот тебе несколько песен, просто кайфовое музло, йоу\n\n"
+    k = 0
+    for i in songs:
+        k += 1
+        if i != songs[-1]:
+            answer += str(k) + ". " + i + "\n"
+        else:
+            answer += str(k) + ". " + i
+
+    return answer
+
+
+# def recomendMore(li, Uid):
+#     n += 3
+#     if n == 21:
+#         print("Нет")
+#     else:
+#         songs = result[n - 3:n]
+#         for i in range(len(songs)):
+#             songs[i] = songs[i][0]
+#
+#         answer = ""
+#         k = 0
+#         for i in songs:
+#             k += 1
+#             if i != songs[-1]:
+#                 answer += str(k) + ". " + i + "\n"
+#             else:
+#                 answer += str(k) + ". " + i
+#
+#     return answer
